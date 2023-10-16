865a6f8d81f13294bdc174bea2f21b73b481364c
Jailton Cruz <jailtoncruz01@gmail.com>
2023-10-15 21:32:26 -0300

    chore: add guards on app module

--- src/server/core/app/app.module.ts
+++ src/server/core/app/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { ModelsModule } from '../../application/models/models.module';
 import { NextJSModule } from '../services/nextjs/nextjs.module';
 import { PrismaModule } from '../services/prisma/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthenticationGuard } from '../../application/modules/authentication/authentication.guard';
+import { RolesGuard } from '../../application/modules/rbac/roles.guard';
 
 @Module({
   imports: [
@@ -14,6 +17,15 @@ import { PrismaModule } from '../services/prisma/prisma.module';
     PrismaModule.forRoot(),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
